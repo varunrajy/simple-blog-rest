08044ffa2ad8d3dbbf360f87ef95da3966f9fdaf
varunrajy <varunraj.y@gmail.com>
2018-07-03 23:07:38 +0530

    port issue

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect("");
+
 app.get("/", (req, res) => {
   res.send("Simple blog application index page");
 });
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log("Listening on port 5000");
 });
